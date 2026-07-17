0b9d851921f01bba7c8d403e8ec0a1a8c7792172
Pasupuleti-Akhila <pasupulatiakhila@gmail.com>
2026-07-17 14:52:46 +0530

    edited master

--- Main.java
+++ Main.java
@@ -5,39 +5,6 @@ public class CharCount
 	public static void main(String[] args) {
 	    
 	    System.out.println(" welcome to github!! ");
-		
-		public void display(){
-			System.out.println("Void Display....!");
-		}
-
-		public void sum(){
-			int a=10;
-			int b=20;
-			int c= a+b;
-			System.out.println(c);
-			System.out.println("sum method executed written by developer-2");
-			System.out.println("this line writtel by developer-1");
-			System.out.println("sum executed ");
-		}
-		public void sub(){
-			int c=5;
-			int d=2;
-			int e= c-d;
-			System.out.println(e);
-		}
-		public void div(){
-			int c=5;
-			int d=2;
-			int e= c/d;
-			System.out.println(e);
-		}
-
-		public void mul(){
-			int c=5;
-			int d=2;
-			int e= c*d;
-			System.out.println(e);
-		}
 
 	}
 }
